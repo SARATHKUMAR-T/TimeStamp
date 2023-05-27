f5ea4972264e84bc6d9089441af2acc9c7795803
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-05-27 10:30:03 +0530

    commit

--- index.js
+++ index.js
@@ -15,18 +15,20 @@ app.get("/date", (req, res) => {
   let currentTimeStamp = `current Time is : ${date.toUTCString().slice(0, -3)}`;
   let time = date.toLocaleTimeString().split("").join("").replaceAll(":", "-");
   var year = date.getFullYear();
-  var month = date.getMonth() + 1; 
+  var month = date.getMonth() + 1;
   var day = date.getDate();
   var seedatetime = month + "." + day + "." + year + "-" + time;
- 
+
   fs.writeFile(`${dirname}/${seedatetime}.txt`, currentTimeStamp, err => {
     if (err) {
       res.send(`Error occurred:${err.message}`);
       return;
     }
-    res.sendFile(path.join(dirname,`${seedatetime}.txt`));
+    res.sendFile(path.join(dirname, `${seedatetime}.txt`));
   });
 });
 
 // Listen to server
-app.listen(8000, () => console.log("server started in : http://localhost:8000/date"));
+app.listen(8000, () =>
+  console.log("server started in : http://localhost:8000/date")
+);
